cf2a8c5048cae34f137a36504b9bfe2db0f0e712
jeanpaulino.ssp <jean.ssparaiso@gmail.com>
2023-01-16 17:51:34 -0300

    att

--- Introducao/Introducao/src/app/app.module.ts
+++ Introducao/Introducao/src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { MeuPrimeiroComponentComponent } from "./meu-primeiro-component/meu-primeiro-component.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MeuPrimeiroComponentComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, MeuPrimeiroComponentComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ Introducao/Introducao/src/app/meu-primeiro-component/meu-primeiro-component.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-meu-primeiro-component",
+  template: ` <p>meu-primeiro-component works!</p> `,
+  styles: [],
+})
+export class MeuPrimeiroComponentComponent {}
