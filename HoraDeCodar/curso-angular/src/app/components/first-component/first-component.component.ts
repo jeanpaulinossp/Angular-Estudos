8bdbd55cf4d5af00c795c9e949d23276def5f60c
jeanpaulinossp <jeanpaulino.ssp@gmail.com>
2023-01-22 17:25:18 -0300

    templates e css

--- HoraDeCodar/curso-angular/src/app/components/first-component/first-component.component.ts
+++ HoraDeCodar/curso-angular/src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit {
+  name: string = 'Jean';
+  age: number = 30;
+  job: string = 'Desenvolvedor';
 
+  constructor() {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
 }
